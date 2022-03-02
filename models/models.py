785f54ea7c2c91934de0df6aac5822e4dbefc60f
Alberto Pérez Fructuoso <albertoperez_135@outlook.es>
2022-03-02 22:42:06 +0100

    Modelos terminados

--- models/models.py
+++ models/models.py
@@ -3,18 +3,55 @@
 from odoo import models, fields, api
 
 
-class apf_sge(models.Model):
-    _name = 'apf_sge.apf_sge'
-    _description = 'apf_sge.apf_sge'
+class Chofer(models.Model):
+    _name = "apf_sge.chofer"
+    _description = "Modelo encargado de la gestion de los chofers"
+    _order = "nombre"
 
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
+    # Relacion uno a muchos entre la tabla chofer y camion
+    chofer_ids = fields.One2many("apf_sge.camion", "camion_id", String = "Chofer")
+
+    nombre = fields.Char(String = "Nombre del chofer", required = True)
+    dni = fields.Char(String = "Documento DNI", required = True)
+    apellidos = fields.Char(String = "Apellidos")
+    fechaNacimiento = fields.Date(String = "Edad")
+    documentosCMR = fields.Char(String = "CMRs o Cartas de porte")
 
 
 
+# TODO usar esto para calcular la edad a partir de la fecha de nacimiento del chofer
 #     @api.depends('value')
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
+class Camion(models.Model):
+    _name = "apf_sge.camion"
+    _description = "Modelo encargado de la gestion de la flota de camiones"
+    _order = "marca"
+
+    modelo = fields.Char(String = "Modelo del camión", required = True)
+    marca = fields.Char(String = "Marca", required = True)
+    matricula = fields.Char(String = "Matrícula")
+    tipoTrailer = fields.Char(String = "Tipo de Trailer")
+
+    # Relacion muchos a uno entre la tabla camion y chofer 
+    camion_id = fields.Many2one("apf_sge.chofer", String = "Vehículo de mercancias")
+
+    # Relacion muchos a muchos entre la tabla camion y destino 
+    destino_ids = fields.Many2many("apf_sge.destino", String = "Destinos de Ruta")
+
+
+
+class Destino(models.Model):
+    _name = "apf_sge.destino"
+    _description = "Modelo encargado de la gestion de los viajes"
+    _order = "name"
+
+    cp = fields.Char(String = "Código postal", required = True)
+    pais = fields.Char(String = "Pais", required = True)
+    direccion = fields.Char(String = "Dirección")
+    provincia = fields.Char(String = "Provincia")
+
+    # Relacion
+    camion_ids = fields.Many2many("apf_sge.camion", String = "Vehículos de mercancias")
